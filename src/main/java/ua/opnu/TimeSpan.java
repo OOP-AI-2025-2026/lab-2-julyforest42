02b30705525d87ea46fdb03a9b224dffcecf7cf6
Sviatoslav Tkachuk <9253222@stud.op.edu.ua>
2025-09-20 21:42:39 +0300

    Local Git

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -3,44 +3,55 @@ package ua.opnu;
 
 public class TimeSpan {
 
-    // TODO: add class fields
+    private int hours;
+    private int minutes;
 
     TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+        this.hours = (hours >= 0 ? hours : 0);
+        this.minutes = (minutes >= 0 && minutes < 60 ? minutes : 0);
     }
 
     int getHours() {
-        return 0;
+        return hours;
     }
 
     int getMinutes() {
-        // TODO: write method body
-        return 0;
+        return minutes;
     }
 
     void add(int hours, int minutes) {
-        // TODO: write method body
+        if (hours < 0 || minutes < 0 || minutes > 59) return;
+        this.hours += hours + (this.minutes + minutes >= 60 ? 1 : 0);
+        this.minutes = (this.minutes + minutes) % 60;
     }
 
     void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+        int hours = timespan.getHours();
+        int minutes = timespan.getMinutes();
+        this.hours += hours + (this.minutes + minutes >= 60 ? 1 : 0);
+        this.minutes = (this.minutes + minutes) % 60;
     }
 
     double getTotalHours() {
-        // TODO: write method body
-        return 0;
+        return hours + (minutes / 60.0);
     }
 
     int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+        return hours * 60 + minutes;
     }
 
     void subtract(TimeSpan span) {
-        // TODO: write method body
+        int hours = span.getHours();
+        int minutes = span.getMinutes();
+        if (hours*60 + minutes > this.hours*60 + this.minutes) return;
+        this.hours -= hours + (minutes > this.minutes ? 1 : 0);
+        this.minutes = (this.minutes - minutes < 0 ? 60 + this.minutes - minutes : this.minutes - minutes);
     }
 
     void scale(int factor) {
-        // TODO: write method body
+        if (factor <= 0) return;
+        int newTime = (this.hours*60 + this.minutes)*factor;
+        this.hours = newTime / 60;
+        this.minutes = newTime % 60;
     }
 }
